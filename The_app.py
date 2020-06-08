d4c4ac801b96a724693e93140463f629d8c6b0cd
NOOB <fool0316@gmail.com>
2020-06-08 12:38:41 +0600

    ai improved

--- The_app.py
+++ The_app.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 import pyttsx3
 import datetime
-import speech_recognition
+import speech_recognition as sr
+import wikipedia
+import webbrowser
 
 
 engiene = pyttsx3.init('sapi5')
 voices = engiene.getProperty('voices')
-print(voices[0].id)
+
 engiene.setProperty('voice', voices[0].id)
 
 def wishuser():
@@ -18,7 +20,26 @@ def wishuser():
 		speak("Good after noon user")
 	else:
 		speak("Good   evening   user")
-	speak("Hi i am gamer bot   how can i help you?")
+	speak("Hi i am your personal assistant how can i help you?")
+
+
+def take_command():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("I am listening")
+		r.pause_threshold = 1
+		audio = r.listen(source)
+	try:
+		print("recognizing")
+		query = r.recognize_google(audio, language='en-in')
+		print(f"user said: {query}\n")
+	except Exception as e:
+		print(e)
+		return "none"
+	return query
+
+
+
 
 
 
@@ -29,6 +50,25 @@ def speak(audio):
 
 if __name__ == "__main__":
 		wishuser()
+		while True:
+			query = take_command().lower()
+
+			if 'wikipedia' in query:
+				speak("I am searching to wikipedia...please wait for sometime")
+
+				query = query.replace("wikipedia", "")
+				results = wikipedia.summary(query, sentences=2)
+				speak("According to wikipedia..")
+				speak(results)
+			elif 'open google' in query:
+				query.replace("open google", "")
+				speak("opening google")
+
+				webbrowser.open("google.com")
+
+
+
+
 
 
 
