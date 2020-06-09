865d0cac6ffcbd90f5a28dc14a1afb50d4bb7b0c
NOOB <fool0316@gmail.com>
2020-06-09 12:42:09 +0600

    ai improved

--- The_app.py
+++ The_app.py
@@ -6,74 +6,88 @@ import wikipedia
 import webbrowser
 import os
 
-
 engiene = pyttsx3.init('sapi5')
 voices = engiene.getProperty('voices')
 
 engiene.setProperty('voice', voices[0].id)
 
-def wishuser():
-	hour = int(datetime.datetime.now().hour)
 
-	if hour>= 0 and  hour<12:
-		speak("Good morning user")
-	elif hour>=12 and hour<18:
-		speak("Good after noon user")
-	else:
-		speak("Good   evening   user")
-	speak("Hi i am your personal assistant how can i help you?")
+def wishuser():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour >= 0 and hour < 12:
+        speak("Good morning user")
+    elif hour >= 12 and hour < 18:
+        speak("Good after noon user")
+    else:
+        speak("Good   evening   user")
+    speak("Hi i am your personal assistant how can i help you?")
 
 
 def take_command():
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("I am listening")
-		r.pause_threshold = 1
-		audio = r.listen(source)
-	try:
-		print("recognizing")
-		query = r.recognize_google(audio, language='en-in')
-		print(f"user said: {query}\n")
-	except Exception as e:
-		print(e)
-		return "none"
-	return query
-
-
-
-
-
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("I am listening")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}\n")
+    except Exception as e:
+        print(e)
+        return "none"
+    return query
 
 
 def speak(audio):
-	engiene.say(audio)
-	engiene.runAndWait()
+    engiene.say(audio)
+    engiene.runAndWait()
+
+
 
 if __name__ == "__main__":
-		wishuser()
-		while True:
-			query = take_command().lower()
+    wishuser()
 
-			if 'wikipedia' in query:
-				speak("I am searching to wikipedia...please wait for sometime")
+    while True:
+        query = take_command().lower()
 
-				query = query.replace("wikipedia", "")
-				results = wikipedia.summary(query, sentences=2)
-				speak("According to wikipedia..")
-				speak(results)
-			elif 'open google' in query:
-				query.replace("open google", "")
-				speak("opening google")
+        if 'wikipedia' in query:
+            speak("I am searching to wikipedia...please wait for sometime")
 
-				webbrowser.open("google.com")
-			elif 'who are you ' in query:
-				speak("i am Nooby bot to help you with your pc and i am your personal assistant")
-				query = query.replace("who are you", "")
-			elif 'how are you' in query:
-				speak("i am doing great sir")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia..")
+            speak(results)
+        elif 'open google' in query:
 
 
+            speak("opening google")
 
+            webbrowser.open("google.com")
+        elif 'who are you' in query:
+
+            speak("i am Nooby bot to help you with your pc and i am your personal assistant")
+            print("iama booooot")
+        elif 'how are you' in query:
+
+            speak("i am doing great sir")
+
+        elif 'discord' in query:
+            speak("i m opening discord please wait for some time.....")
+            discord = "C:\\Users\\User\\AppData\\Local\\Discord\\app-0.0.306\\Discord.exe"
+            os.startfile(discord)
+        elif 'close app' in query:
+            os.system('TASKILL /F /IM discord.exe')
+
+        elif 'time' in query:
+            strtime = datetime.datetime.now().strftime("%H:%M")
+            speak(strtime)
+            print(strtime)
+        elif 'exit' in query:
+            speak("i am very happy to serve you")
+            speak("you can call me anytime")
+            exit()
 
 
 
